6908c26486cf42c71a6a5002170f948606651a5d
james <alphafoobar@users.noreply.github.com>
2019-10-17 19:21:56 +1300

    Parse JSON messages

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::env;
+use std::str;
 
-use chrono::Utc;
+use chrono::{DateTime, Utc};
+use serde_json::Value;
 use websocket::ClientBuilder;
+use websocket::ws::dataframe::DataFrame;
 
 fn main() {
     let key = "WEBSOCKET_ADDRESS";
@@ -9,14 +12,28 @@ fn main() {
         Ok(address) => {
             let mut client = ClientBuilder::new(address.as_str())
                 .unwrap()
-                .connect_secure(None)
+                .connect_insecure()
+//                .connect_secure(None)
                 .unwrap();
 
             let mut counter: u128 = 0;
             for message in client.incoming_messages() {
                 // Skip some messages.
-                if counter % 10 == 0 {
-                    println!("{:?}: -> {:?}", Utc::now(), &message.unwrap());
+                let text = message.unwrap().take_payload();
+                let s = str::from_utf8(&text.as_slice()).unwrap();
+                let v: Value = serde_json::from_str(&s).unwrap();
+                let event_type = v["event"].as_str();
+                if event_type.is_some() && event_type.unwrap().eq("DISCONNECT") {
+                    println!("{:?}: [{}] -> DISCONNECT {}", Utc::now(), counter, s);
+                } else {
+                    let rate = v["rate"].as_str();
+                    let time = v["timestamp"].as_str();
+                    if rate.is_some() && time.is_some() {
+                        let result = DateTime::parse_from_rfc3339(time.unwrap());
+                        let now = Utc::now();
+                        let lag = ((now.timestamp_nanos() - result.unwrap().timestamp_nanos()) as f64) / 1000000.0;
+                        println!("{:?}: [{}] -> rate={}, time={}, lag={}", now, counter, rate.unwrap(), time.unwrap(), lag);
+                    }
                 }
                 counter = counter + 1;
             }
