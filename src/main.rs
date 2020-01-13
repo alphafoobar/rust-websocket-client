c42ff812b4ec604801e8bf0c926bd8521cba770c
james <alphafoobar@users.noreply.github.com>
2020-01-14 12:52:45 +1300

    Allow functions to return Vectors

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,25 @@ fn main() {
     let key = "WEBSOCKET_ADDRESS";
     match env::var(key) {
         Ok(address) => {
-            let address = address.as_str();
-            let mut client = ClientBuilder::new(address).unwrap().connect(None).unwrap();
-
+            let mut client = ClientBuilder::new(address.as_str()).unwrap().connect(None).unwrap();
             let mut previous = Utc::now();
             let mut counter: u128 = 0;
             println!("{:?}: Connected", Utc::now());
+
             for message in client.incoming_messages() {
                 // Skip some messages.
                 let text = message.unwrap().take_payload();
                 let s = str::from_utf8(&text.as_slice()).unwrap();
                 let v: Value = serde_json::from_str(&s).unwrap();
                 let event_type = v["event"].as_str();
+
                 if event_type.is_some() && event_type.unwrap().eq("DISCONNECT") {
                     println!("{:?}: [{}] -> DISCONNECT {}", Utc::now(), counter, s);
                 } else {
                     let rate = v["rate"].as_str();
                     let time = v["timestamp"].as_str();
                     let constituents = v["constituents"].as_array();
+
                     if rate.is_some() && time.is_some() && constituents.is_some() {
                         let result = DateTime::parse_from_rfc3339(time.unwrap());
                         let now = Utc::now();
@@ -65,25 +66,23 @@ fn main() {
     }
 }
 
-fn map(vs: Vec<Value>) -> [String; 3] {
-    let mut xs: [String; 3] = ["".to_string(), "".to_string(), "".to_string()];
-    for i in 0..3 {
-        let v = vs[i]
-            .as_object()
+fn map(vs: Vec<Value>) -> Vec<String> {
+    let mut xs = Vec::new();
+    for i in vs.iter() {
+        let v = i.as_object()
             .unwrap()
             .get("midPrice")
             .unwrap()
             .as_str()
             .unwrap();
-        xs[i] = v.to_string();
+        xs.push(v.to_string());
     }
     return xs;
 }
 
 fn algorithm_name(vs: Vec<Value>) -> String {
-    for i in 0..3 {
-        let option = vs[i]
-            .as_object()
+    for i in vs.iter() {
+        let option = i.as_object()
             .unwrap()
             .get("algorithmName");
 
@@ -100,15 +99,13 @@ fn algorithm_name(vs: Vec<Value>) -> String {
 
 fn propagation_delay(vs: Vec<Value>) -> DateTime<Utc> {
     let mut latest: DateTime<Utc> = Utc.ymd(1970, 1, 1).and_hms_milli(0, 0, 0, 0);
-    for i in 0..3 {
-        let ts = DateTime::parse_from_rfc3339(
-            vs[i]
-                .as_object()
-                .unwrap()
-                .get("lastUpdatedTimestamp")
-                .unwrap()
-                .as_str()
-                .unwrap(),
+    for i in vs.iter() {
+        let ts = DateTime::parse_from_rfc3339(i.as_object()
+                                                  .unwrap()
+                                                  .get("lastUpdatedTimestamp")
+                                                  .unwrap()
+                                                  .as_str()
+                                                  .unwrap(),
         ).unwrap().with_timezone(&Utc);
 
         if ts.gt(&latest) {
